63ac069c178eaf56a066c70db189b59c65e49b1c
ZairBalam <fzairb2@gmail.com>
2025-11-22 19:22:30 -0600

    feat: define asset and font folders in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  // Este es un StatelessWidget porque este widget no necesita guardar ni actualizar estado interno. Su apariencia depende únicamente de los parámetros de entrada.
+
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(theme: AppTheme.lightTheme, home: Container());
   }
 }
