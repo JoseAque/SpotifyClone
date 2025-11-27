c0ceed0f09628d285c22755862f1e0025398daaa
JoseAque <jose.aque21@hotmail.com>
2025-11-27 16:29:31 -0600

    feat(auth): update imports and fix variable names in authentication services feat(firebase): add cloud_firestore dependency and register plugin

--- lib/data/reporitory/auth/auth_repository_impl.dart
+++ lib/data/reporitory/auth/auth_repository_impl.dart
@@ -3,7 +3,7 @@ import 'package:spotify/data/models/auth/create_user_req.dart';
 import 'package:spotify/data/models/auth/signin_user_req.dart';
 import 'package:spotify/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify/domain/repository/auth/auth.dart';
-import 'package:spotify/service_locator.dart';
+import '../../../service_locator.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
   @override
--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify/data/models/auth/create_user_req.dart';
@@ -5,21 +6,23 @@ import 'package:spotify/data/models/auth/signin_user_req.dart';
 
 abstract class AuthFirebaseService {
   Future<Either> signup(CreateUserReq createUserReq);
-  Future<Either> signin(SigninUserReq siginUserReq);
+
+  Future<Either> signin(SigninUserReq signinUserReq);
 }
 
 class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
-  Future<Either> signin(SigninUserReq siginUserReq) async {
+  Future<Either> signin(SigninUserReq signinUserReq) async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: siginUserReq.email,
-        password: siginUserReq.password,
+        email: signinUserReq.email,
+        password: signinUserReq.password,
       );
 
-      return const Right('Signin was Successfull');
+      return const Right('Signin was Successful');
     } on FirebaseAuthException catch (e) {
       String message = '';
+
       if (e.code == 'invalid-email') {
         message = 'Not user found for that email';
       } else if (e.code == 'invalid-credential') {
@@ -33,18 +36,24 @@ class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
   Future<Either> signup(CreateUserReq createUserReq) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: createUserReq.email,
         password: createUserReq.password,
       );
 
-      return Right('Signup was Successfull');
+      FirebaseFirestore.instance.collection('Users').doc(data.user?.uid).set({
+        'name': createUserReq.fullName,
+        'email': data.user?.email,
+      });
+
+      return const Right('Signup was Successful');
     } on FirebaseAuthException catch (e) {
       String message = '';
+
       if (e.code == 'weak-password') {
         message = 'The password provided is too weak';
       } else if (e.code == 'email-already-in-use') {
-        message = 'An account already exist with that email.';
+        message = 'An account already exists with that email.';
       }
 
       return Left(message);
