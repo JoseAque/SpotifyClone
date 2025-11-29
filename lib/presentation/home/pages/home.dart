aa0144ce76d1ea92b6775a3c6255f1df607de37e
ZairBalam <fzairb2@gmail.com>
2025-11-28 20:12:56 -0600

    feat(home): add HomePage with tab navigation and top card layout feat(appbar): add option to hide back button in BasicAppbar feat(assets): add new image and vector assets for home page refactor(auth): update navigation to HomePage from Signin and Signup pages refactor: remove unused RootPage

---
+++ lib/presentation/home/pages/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+
+import '../../../common/widgets/button/appbar/app_bar.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BasicAppbar(
+        hideBack: true,
+        title: SvgPicture.asset(AppVectors.logo, height: 40, width: 40),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [_homeTopCard(), _tabs()],
+        ),
+      ),
+    );
+  }
+
+  Widget _homeTopCard() {
+    return Center(
+      child: SizedBox(
+        height: 140,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SvgPicture.asset(AppVectors.homeTopCard),
+            ),
+            Align(
+              alignment: AlignmentGeometry.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 60),
+                child: Image.asset(AppImages.homeArtist),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tabs() {
+    return TabBar(
+      controller: _tabController,
+      tabs: [Text('News'), Text('Videos'), Text('Artist'), Text('Podcast')],
+    );
+  }
+}
